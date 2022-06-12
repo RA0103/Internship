5509deadfbab3a5908c68bec413864e35efaf7e6
RA0103 <rakhmonovra@gmail.com>
2022-06-13 04:15:29 +0500

    done

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../Footer";
-import { Container, Wrapper } from "./styles";
+import { Container, Info, Wrapper } from "./styles";
+import { Input, Button, Checkbox } from "../Generic";
+import { useMutation } from "react-query";
 
 export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { REACT_APP_BASE_URL: url } = process.env;
+
+  const { mutate } = useMutation(() => {
+    return fetch(`${url}/public/auth/login`, {
+      method: "POST",
+      headers: {
+        'Content-type': 'Application/json'
+      }, 
+      body: JSON.stringify({
+        email,
+        password
+      })
+    }).then((res) => res.json())
+  });
+
+  const onSubmit = () => {
+    console.log(email, password);
+  };
+
   return (
     <>
       <Container>
-        <Wrapper>test</Wrapper>
+        <Wrapper>
+          <div className='title'>Sign In</div>
+          <Input
+            onChange={({ target: { value } }) => setEmail(value)}
+            value={email}
+            width={"100%"}
+            style={{ border: "none", borderBottom: "1px solid #E6E9EC" }}
+            mt={60}
+            placeholder={"Email"}
+          />
+          <Input
+            onChange={({ target: { value } }) => setPassword(value)}
+            value={password}
+            style={{ border: "none", borderBottom: "1px solid #E6E9EC" }}
+            mt={40}
+            placeholder={"Password"}
+          />
+          <Info>
+            <Checkbox>Remember me</Checkbox>
+            <a href='/signIn'>Forgot</a>
+          </Info>
+          <Button mt={32} type={"primary"} onClick={onSubmit}>
+            Login
+          </Button>
+        </Wrapper>
       </Container>
       <Footer />
     </>
--- src/components/SignIn/styles.js
+++ src/components/SignIn/styles.js
@@ -9,12 +9,20 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 580px;
-  height: 376px;
+  height: fit-content;
   background: #ffffff;
   border: 1px solid #e6e9ec;
   box-shadow: 0px 10px 30px rgba(13, 38, 59, 0.05);
   border-radius: 3px;
+  padding: 24px 36px;
 `;
 
-export { Container, Wrapper };
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 17px;
+`;
+
+export { Container, Wrapper, Info };
